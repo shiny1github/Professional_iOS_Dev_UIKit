a8f39bab862559e1077eae516d1de7b9242eaa34
shiny1github <shivanisuman298@gmail.com>
2024-10-05 06:12:12 +0530

    Set Up the Entry Point Programmatically

--- Professional_iOS_Dev_UIKit/AppDelegate.swift
+++ Professional_iOS_Dev_UIKit/AppDelegate.swift
@@ -9,12 +9,23 @@ import UIKit
 
 
 
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let initialViewController = ViewController() // Replace with your actual view controller
+        window?.rootViewController = initialViewController
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
 
 
     
 
 
-}
+
 
